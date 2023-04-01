f8ff74fcb3a8ad2b78d58fc25510839a6508c16f
Ed-Gah <edwingah2021@gmail.com>
2023-04-01 18:32:38 +0100

    Details

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -10,21 +10,29 @@ function HomePage() {
       title: "BMW electric series",
       description:
         "The latest confort you can possibly get in this mordern era",
-      imageUrl: "https://cdn.arstechnica.net/wp-content/uploads/2022/11/2023-BMW-i7-exterior-1-800x600.jpg",
+      imageUrl:
+        "https://cdn.arstechnica.net/wp-content/uploads/2022/11/2023-BMW-i7-exterior-1-800x600.jpg",
       link: "details",
+      engine: "Diesel",
+      type: "GLE 350",
+      fuelType: "Diesel",
+      mileage: "18 mpg city & 23 mpg highway",
+      price: "38,000.00",
     },
     {
       title: "Tesla Model 3",
       description:
         "The latest confort you can possibly get in this mordern era",
-      imageUrl: "https://media.ed.edmunds-media.com/tesla/model-3/2022/oem/2022_tesla_model-3_sedan_performance_fq_oem_1_815.jpg",
+      imageUrl:
+        "https://media.ed.edmunds-media.com/tesla/model-3/2022/oem/2022_tesla_model-3_sedan_performance_fq_oem_1_815.jpg",
       link: "details",
     },
     {
       title: "Mecedes 2021",
       description:
         "The latest confort you can possibly get in this mordern era",
-      imageUrl: "https://cdn.motor1.com/images/mgl/k3pee/s3/mercedes-c-klasse-limousine-2021.webp",
+      imageUrl:
+        "https://cdn.motor1.com/images/mgl/k3pee/s3/mercedes-c-klasse-limousine-2021.webp",
       link: "details",
     },
     {
@@ -38,60 +46,70 @@ function HomePage() {
       title: "G-Wagon",
       description:
         "The latest confort you can possibly get in this mordern era",
-      imageUrl: "https://images.drive.com.au/driveau/image/upload/c_fill,f_auto,g_auto,h_540,q_auto:good,w_960/cms/uploads/nvptezjzuppfwmj6jpad",
+      imageUrl:
+        "https://images.drive.com.au/driveau/image/upload/c_fill,f_auto,g_auto,h_540,q_auto:good,w_960/cms/uploads/nvptezjzuppfwmj6jpad",
       link: "details",
     },
     {
       title: "Range Rover",
       description:
         "The latest confort you can possibly get in this mordern era",
-      imageUrl: "https://i.gaw.to/content/photos/55/21/552190-land-rover-range-rover-2023-la-cinquieme-generation-du-premier-vus-de-luxe.jpeg",
+      imageUrl:
+        "https://i.gaw.to/content/photos/55/21/552190-land-rover-range-rover-2023-la-cinquieme-generation-du-premier-vus-de-luxe.jpeg",
       link: "details",
     },
-    
-   
   ];
   return (
-    <div className=" 2xl:w-[60%] mx-auto xl:w-[60%] lg:w-[80%] md:w-[95%] sm:w-[100%]">
-      <div>
-        <NavBar />
-      </div>
-      <div className=" grid grid-flow-row grid-cols-2  mt-20 ">
+    <div className="bg-red-900">
+      <div className=" 2xl:w-[60%] mx-auto xl:w-[60%] lg:w-[80%] md:w-[95%] sm:w-[100%]">
         <div>
-          <h1 className="vision-txt">{`We don't just sell cars we sell a dream to you`}</h1>
-          <div className=" grid grid-flow-row grid-cols-1 gap-4 mt-10">
-            {/* <button className="sell">Buy</button> */}
-            <button className="sell">Sell</button>
+          <NavBar />
+        </div>
+        <div className=" grid grid-flow-row grid-cols-2  mt-20 ">
+          <div>
+            <h1 className="vision-txt">{`We don't just sell cars we sell a dream to you`}</h1>
+            <div className=" grid grid-flow-row grid-cols-1 gap-4 mt-10">
+              {/* <button className="sell">Buy</button> */}
+              <button className="sell">Sell</button>
+            </div>
+            <div className=" grid grid-flow-row grid-cols-2 gap-4 mt-16">
+              <img src="../assets/images/car1.png" />
+              <img src="../assets/images/car2.png" />
+            </div>
           </div>
-          <div className=" grid grid-flow-row grid-cols-2 gap-4 mt-16">
-            <img src="../assets/images/car1.png" />
-            <img src="../assets/images/car2.png" />
+          <div>
+            <img src="../assets/images/background-image.png" />
+            <div className=" ml-20 flex justify-between mr-10 lg:mt-20 xl:mt-8">
+              <img src="../assets/images/icon1.png" />
+              <img src="../assets/images/icon2.png" />
+              <img src="../assets/images/icon3.png" />
+              <img src="../assets/images/icon4.png" />
+            </div>
           </div>
         </div>
-        <div>
-          <img src="../assets/images/background-image.png" />
-          <div className=" ml-20 flex justify-between mr-10 lg:mt-20 xl:mt-8">
-            <img src="../assets/images/icon1.png" />
-            <img src="../assets/images/icon2.png" />
-            <img src="../assets/images/icon3.png" />
-            <img src="../assets/images/icon4.png" />
-          </div>
+        <div className=" carcart my-20 bg-[var(--text-300)]">
+          {carsData.map((car: any, i: any) => (
+            <div key={i}>
+              <CarCart
+                title={car.title}
+                description={car.description}
+                imageUrl={car.imageUrl}
+                onClick={() => {
+                  console.log("The car id index: " + i);
+                  router.push({
+                    pathname: "/dashboard/car-details",
+                    query: car,
+                  });
+                }}
+                engine={car.engine}
+                type={car.type}
+                fuelType={car.fuelType}
+                mileage={car.mileage}
+                price={`$${car.price}`}
+              />
+            </div>
+          ))}
         </div>
-      </div>
-      <div className=" carcart my-20">
-        {carsData.map((car: any, i: any) => (
-          <div key={i}>
-            <CarCart
-              title={car.title}
-              description={car.description}
-              imageUrl={car.imageUrl}
-              onClick={() => {
-                console.log("The car id index: " + i);
-                router.push({ pathname: "/dashboard/car-details", query: car });
-              }}
-            />
-          </div>
-        ))}
       </div>
     </div>
   );
--- src/components/dashboard/CarCart.tsx
+++ src/components/dashboard/CarCart.tsx
@@ -5,6 +5,11 @@ export default function CarCart({
   description,
   imageUrl,
   onClick,
+  engine,
+  type,
+  fuelType,
+  mileage,
+  price,
 }: any) {
   return (
     <div>
@@ -25,32 +30,32 @@ export default function CarCart({
         </a>
         <div className="p-5">
           <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {title}
+            <h5 className=" font-medium text-[var(--secondary-900)] text-lg">
+              Price: {price}
             </h5>
           </a>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Name: {title}
+          </p>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Engine: {engine}
+          </p>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Type: {type}
+          </p>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Fuel Type: {fuelType}
+          </p>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Mileage: {mileage}
+          </p>
+
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             {description}
           </p>
-          <a
-            href="#"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Read more
-            <svg
-              aria-hidden="true"
-              className="w-4 h-4 ml-2 -mr-1"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </a>
+          <button className=" bg-black w-full text-[var(--text-400)] font-medium text-[18px] rounded-lg py-2">
+            Place off your bid
+          </button>
         </div>
       </div>
     </div>
