29184bf75f0ce5c388977dcaf57b094bead65e22
Ed-Gah <edwingah2021@gmail.com>
2023-04-01 17:34:21 +0100

    imaged modification

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -7,45 +7,45 @@ function HomePage() {
   const router = useRouter();
   const carsData = [
     {
-      title: "Mecedes 2021",
+      title: "BMW electric series",
       description:
         "The latest confort you can possibly get in this mordern era",
-      imageUrl: "https://cdn.wallpapersafari.com/81/69/cbQoSy.jpg",
+      imageUrl: "https://cdn.arstechnica.net/wp-content/uploads/2022/11/2023-BMW-i7-exterior-1-800x600.jpg",
+      link: "details",
+    },
+    {
+      title: "Tesla Model 3",
+      description:
+        "The latest confort you can possibly get in this mordern era",
+      imageUrl: "https://media.ed.edmunds-media.com/tesla/model-3/2022/oem/2022_tesla_model-3_sedan_performance_fq_oem_1_815.jpg",
       link: "details",
     },
     {
       title: "Mecedes 2021",
       description:
         "The latest confort you can possibly get in this mordern era",
-      imageUrl: "https://cdn.wallpapersafari.com/81/69/cbQoSy.jpg",
+      imageUrl: "https://cdn.motor1.com/images/mgl/k3pee/s3/mercedes-c-klasse-limousine-2021.webp",
       link: "details",
     },
     {
       title: "Mecedes 2021",
       description:
         "The latest confort you can possibly get in this mordern era",
-      imageUrl: "https://cdn.wallpapersafari.com/81/69/cbQoSy.jpg",
+      imageUrl: "https://mcdn.wallpapersafari.com/medium/76/6/JqVOtk.jpg",
       link: "details",
     },
     {
-      title: "Mecedes 2021",
+      title: "G-Wagon",
       description:
         "The latest confort you can possibly get in this mordern era",
-      imageUrl: "https://cdn.wallpapersafari.com/81/69/cbQoSy.jpg",
+      imageUrl: "https://images.drive.com.au/driveau/image/upload/c_fill,f_auto,g_auto,h_540,q_auto:good,w_960/cms/uploads/nvptezjzuppfwmj6jpad",
       link: "details",
     },
     {
-      title: "Mecedes 2021",
+      title: "Range Rover",
       description:
         "The latest confort you can possibly get in this mordern era",
-      imageUrl: "https://cdn.wallpapersafari.com/81/69/cbQoSy.jpg",
-      link: "details",
-    },
-    {
-      title: "Mecedes 2021",
-      description:
-        "The latest confort you can possibly get in this mordern era",
-      imageUrl: "https://cdn.wallpapersafari.com/81/69/cbQoSy.jpg",
+      imageUrl: "https://i.gaw.to/content/photos/55/21/552190-land-rover-range-rover-2023-la-cinquieme-generation-du-premier-vus-de-luxe.jpeg",
       link: "details",
     },
     
