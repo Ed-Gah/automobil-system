a7c5a3ef57d58814fe695c9b7df52773400e9267
Ed-Gah <edwingah2021@gmail.com>
2023-04-03 08:58:09 +0100

    Seller stuff fixed

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { Registration } from "@/src/components/seller";
 import { CarCart, Marketing, NavBar } from "@/src/components/dashboard";
 
 function HomePage() {
